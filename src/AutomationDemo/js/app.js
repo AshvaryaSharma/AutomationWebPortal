a34b2b9a9036056f2856a08d527d2c23e67e3369
AjayPoshak <poshakajay@gmail.com>
2015-11-04 00:07:01 +0530

    Dummy Commit

---
+++ src/AutomationDemo/js/app.js
@@ -0,0 +1,16 @@
+var app = angular.module("MyBlog",['ngRoute']);
+console.log("Inside app.js...");
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+      when('/index', {
+        templateUrl: 'partials/blog-home.html',
+        controller: 'HomeController'
+      }).
+	  when('/link',{
+		templateUrl: 'partials/full-link.html',
+		controller: 'LinkController'		
+	  }).
+      otherwise({
+        redirectTo: '/index'
+      });
+}]);
