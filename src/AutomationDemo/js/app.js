689a9e70d4172a700ed286eec3a87fe9d1bc33d8
AjayPoshak <poshakajay@gmail.com>
2015-11-27 11:10:52 +0530

    Included the dependency for angucomplete

--- src/AutomationDemo/js/app.js
+++ src/AutomationDemo/js/app.js
@@ -1,10 +1,11 @@
 'use strict';
-var app = angular.module('safe-app',['ngRoute','checklist-model','safeServices','ngSanitize','ui.select']);
+var app = angular.module('safe-app',['ngRoute','checklist-model','safeServices','ngSanitize','ui.select','angucomplete-alt']);
 app.config(['$routeProvider', function($routeProvider){
 	console.log("Inside router");
 	$routeProvider.
 	when('/dashboard', {
 		templateUrl : 'partials/dashboard.html',
+		//resolve : dashboardController.resolve,
 		controller: 'dashboardController'
 	}).
 	when('/viewTestcases', {
