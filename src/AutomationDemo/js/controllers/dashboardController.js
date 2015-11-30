aee5e36615fcdf813585e661091e4066ed46c924
HimNuevo <nuevothoughts.himanshu@gmail.com>
2015-11-30 10:05:19 +0530

    Added the code for sessionStorage

--- src/AutomationDemo/js/controllers/dashboardController.js
+++ src/AutomationDemo/js/controllers/dashboardController.js
@@ -1,22 +1,29 @@
-/*This will act like a Home Controller*/
+ /*
+	@Author: Ajay Poshak
+	@Date: 29 Nov 2015
+	@Purpose : This controller is responsible for dashboard.html
+*/
 'use strict';
-app.controller('dashboardController', function(createTestService, $scope, $rootScope) {
-	$rootScope.isLogged=false;
-	var current = $rootScope.current = {};
-	$rootScope.safeUserData = {};
-	console.log("Inside Dashboard Controller...");
-	
-	 function getUserDetails() {
-		console.log("Getting user data...");
-		createTestService.getUserData().then(function(appData) {
-			$scope.safeUserData = appData;
-			console.log("******User Data::"+$scope.safeUserData.userDetails.first_name);
-			$rootScope.safeUserData = $scope.safeUserData;
-		});
-		$rootScope.isLogged=true;
+angular.module("safe-app")
+	.controller('dashboardController', dashboardController);
+	/*
+	**@function dashbaordController:- This is the controller for dashboard.html
+	*/
+	function dashboardController(createTestService, $scope, $rootScope, SessionKeeper) {
+		console.log("Inside Dashboard Controller...");
+		/*
+		** @function getUserDetails :- This function will get the user related information by calling the service. And set that information into rootScope and session so that it can be shared across the application. 
+		*/
+		 function getUserDetails() {
+			console.log("Getting user data...");
+			var current = $rootScope.current;
+			createTestService.getUserData().then(function(appData) {
+				$scope.safeUserData = appData;
+				console.log("******User Data::"+$scope.safeUserData.userDetails.first_name);
+				$rootScope.current = $scope.safeUserData;
+				SessionKeeper.save();
+			});
+			$rootScope.isLogged=true;
+		};
+		getUserDetails();
 	};
-	
-	getUserDetails();
-	//console.log("=========>User Data::"+current.safeUserData.userDetails);
-	
-});
