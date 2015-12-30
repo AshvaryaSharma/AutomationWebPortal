9471151a357ff066f80237afb97f44f115e680e2
AjayPoshak <AP00359667@TechMahindra.com>
2015-12-30 16:20:41 +0530

    Empty Structure

---
+++ src/AutomationDemo/js/controllers/createTestsuiteController.js
@@ -0,0 +1,7 @@
+'use strict';
+angular.module("safe-app")
+	.controller("createTestsuiteController", createTestsuiteController);
+	
+function createTestsuiteController($scope){
+	console.log("Inside create test suite nodule...");
+};
