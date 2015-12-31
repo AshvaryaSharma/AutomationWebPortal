97e81be882d18e0da4741ac09d86b2d1f3365d21
AjayPoshak <AP00359667@TechMahindra.com>
2015-12-31 22:15:17 +0530

    Updated controller

--- src/AutomationDemo/js/controllers/createTestsuiteController.js
+++ src/AutomationDemo/js/controllers/createTestsuiteController.js
@@ -2,6 +2,66 @@
 angular.module("safe-app")
 	.controller("createTestsuiteController", createTestsuiteController);
 	
-function createTestsuiteController($scope){
-	console.log("Inside create test suite nodule...");
+function createTestsuiteController(createTestService, $scope, $http, $rootScope, SessionKeeper){
+	console.log("Inside create test suite module...");
+	var current = $rootScope.current;
+	if (!(current))
+		current = $rootScope.current = SessionKeeper.read();
+	//console.log("Previous Controller::"+current.userDetails.first_name);
+	/*
+	**@function getAllApplications :- It will get the data of all allowed applications for the user.
+	@param user_id :- Using the user id, the list of allowed applications will be retrieved.
+	*/
+	function getAllApplications() {
+		createTestService.getAppData()
+		.then(function(appData) {
+			$rootScope.current = $scope.safeAppData = appData;
+			//console.log("Application Data::"+$rootScope.current.applicationList[0].app_name);
+			//SessionKeeper.save();
+		})
+		.catch(function(appData){
+			$scope.errorStatus = true;
+			$scope.errorMessage = "Not able to get application data";
+		});
+	}
+	/*
+	** @function appSelectionEvent:- This function will be called when the user selects an application in view.
+	*/
+	$scope.appSelectionEvent = function() {
+		$scope.isApplicationSelected = true;
+		console.log("isApplicationSelected::"+$scope.isApplicationSelected);
+		loadTestSuites();
+	}
+	/*
+	**@function loadTestSuites : This function will load the test suites from services.
+	*/
+	function loadTestSuites() {
+		console.log("Loading the test suites...");
+		createTestService.getTestsuiteByAppAndGroup()
+		.then(function(appData){
+			$rootScope.current = $scope.testSuiteData = appData.testSuite;
+		})
+		.catch(function(appData){
+			$scope.errorStatus = true;
+			$scope.errorMessage = 'Test Suite data not available';
+		});
+		
+	}
+	
+	/*
+	**@function startApplication: Starting function of the page which gets called every page is loaded.
+	*/
+	function startApplication() {
+		if(current == null || current == undefined){
+			getUserDetails();
+			console.log("Calling Services....");
+		}
+		else {
+			$scope.safeUserData = current.userDetails;
+		}
+		getAllApplications();
+		
+	}
+	startApplication();
+	SessionKeeper.save();
 };
